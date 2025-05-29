f7331e6b93e83df69ac57b3c622e757dee065106
Sebastián Cano Rincón <sebastiancano21@hotmail.es>
2025-05-28 19:45:09 -0500

    ✍🏼Update parquet file path to use COVID-19 data and fix print statement typo

--- spark_jobs/validate_parquet.py
+++ spark_jobs/validate_parquet.py
@@ -3,10 +3,10 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName("ValidateParquet").getOrCreate()
 
 # Leer el Parquet
-df = spark.read.parquet("s3://proyecto3bigdata/trusted/joined_weather_uber_multiyear/")
+df = spark.read.parquet("s3://proyecto3bigdata/trusted/joined_weather_covid_multiyear/")
 
 # Mostrar cantidad de registros
-print("N  mero de filas:", df.count())
+print("Numero de filas:", df.count())
 
 # Mostrar algunas filas
 df.show(10)

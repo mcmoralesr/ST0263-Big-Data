6cdb881c737b8ede2a47f71a644d5cb905f6bef3
Sebastián Cano Rincón <sebastiancano21@hotmail.es>
2025-05-28 19:42:53 -0500

    📌Refactor ETL process to integrate COVID-19 data, update variable names, and improve data filtering and output messages

--- spark_jobs/etl_spark_multi_year.py
+++ spark_jobs/etl_spark_multi_year.py
@@ -1,35 +1,48 @@
 # etl_spark_multiyear.py
 
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, year, to_date, regexp_extract, avg
+from pyspark.sql.functions import col, year, to_date, avg, count as spark_count, when
 from pyspark.sql.types import DoubleType
 
-spark = SparkSession.builder.appName("ETLMultiYearWeatherUber").getOrCreate()
-
+spark = SparkSession.builder.appName("ETLMultiYearWeatherCovid").getOrCreate()
+spark.conf.set("spark.sql.legacy.timeParserPolicy", "LEGACY")
 # Leer todos los archivos de clima
 weather_df = spark.read.option("header", True).csv("s3://proyecto3bigdata/raw/weather/*.csv")
 
 # Limpiar y convertir columnas
 weather_df = weather_df \
-    .withColumn("date", to_date(col("date"), "yyyy-MM-dd HH:mm:ss")) \
+    .withColumn("date", to_date(col("date"), "yyyy-MM-dd")) \
     .withColumn("tavg", col("tavg").cast(DoubleType())) \
     .withColumn("year", year(col("date")))
 
 # Filtrar valores v  lidos
-weather_df = weather_df.filter(col("tavg").isNotNull())
+weather_df = weather_df.filter(col("tavg").isNotNull() & col("year").isNotNull())
 
 # Promedio anual de temperatura
 weather_avg = weather_df.groupBy("year").agg(avg("tavg").alias("avg_temp"))
 
-# Leer archivo Uber
-uber_df = spark.read.option("header", True).csv("s3://proyecto3bigdata/raw/uber/Travel_Times.csv")
+# Leer archivo COVID
+covid_df = spark.read.option("header", True).csv("s3://proyecto3bigdata/raw/covid19/*.csv")
 
-# Extraer a  o de rango de fechas con RegEx
-uber_df = uber_df.withColumn("date_range", regexp_extract(col("Date Range"), r"(\d{4})", 1))
-uber_df = uber_df.withColumn("year", col("date_range").cast("int"))
+# Datos COVID19
+covid_df = covid_df.filter(col("Date_of_Infection").isNotNull())
+covid_df = covid_df.withColumn("Date_of_Infection", to_date(col("Date_of_Infection"), "yyyy-MM-dd"))
+covid_df = covid_df.withColumn("year", year(col("Date_of_Infection")))
+covid_df = covid_df.filter(col("year").isNotNull())
 
-# Join
-joined_df = uber_df.join(weather_avg, on="year", how="inner")
+#Estadisticas COVID por año
+covid_stats = covid_df.groupBy("year").agg(
+    spark_count("Patient_ID").alias("total_cases"),
+    spark_count(when(col("Hospitalized") == "Yes", 1)).alias("hospitalized_cases"),
+    spark_count(when(col("Severity") == "Severe", 1)).alias("severe_cases"),
+    spark_count(when(col("ICU_Admission") == "Yes", 1)).alias("icu_cases")
+)
+
+print("Estadisticas COVID 19 por año: ")
+covid_stats.orderBy("year").show()
+
+# Join entre COVID y clima
+joined_df = covid_stats.join(weather_avg, on="year", how="inner")
 
 print("Esquema del resultado:")
 joined_df.printSchema()
@@ -38,13 +51,13 @@ print("Ejemplo de datos:")
 joined_df.show(5)
 
 row_count = joined_df.count()
-print("Total de filas a escribir:", row_count)
+print(f"Total de filas a escribir > {row_count}")
 
 # Guardar a S3 si hay datos
 if row_count > 0:
-    joined_df.write.mode("overwrite").parquet("s3://proyecto3bigdata/trusted/joined_weather_uber_multiyear/")
-    print(" ^|^e Parquet guardado exitosamente.")
+    joined_df.write.mode("overwrite").parquet("s3://proyecto3bigdata/trusted/joined_weather_covid_multiyear/")
+    print("Parquet guardado exitosamente.")
 else:
-    print(" ^z   ^o No hay datos para guardar.")
+    print("No hay datos para guardar.")
 
 spark.stop()
